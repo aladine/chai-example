e4d32afaa054fc481808867985a7712ef491d86e
aladine <dan.aladine@gmail.com>
2014-11-08 22:46:58 +0800

    update

---
+++ tests/tests.js
@@ -0,0 +1,22 @@
+var assert = chai.assert;
+// Sample test
+describe('Array', function(){
+  
+ describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      var result;
+        var cal = new Cal();
+        result = cal.add(4, 5);
+        return assert.equal(result, 9);
+    });
+    it('should return subtract is -1',function(){
+    	var result; 
+    	var cal = new Cal();
+    	result = cal.subtract(4, 5);
+		return assert.equal(result, -1);
+    })
+  });
+  describe('#indexOf()', function(){
+  	
+  });
+});
